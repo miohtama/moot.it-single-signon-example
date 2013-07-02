67f48a30e2a066bdfa1d86db46f002e6209ade86
Mikko Ohtamaa <mikko@opensourcehacker.com>
2013-07-02 10:41:22 +0300

    Dump out user config

--- mootexample/views.py
+++ mootexample/views.py
@@ -60,6 +60,7 @@ def create_moot_config(request):
 
     # user config
     config = dict(id=user, displayname=displayname, avatar="http://foobar", is_admin=is_admin, email=email)
+    print "Config %s" % json.dumps(config)
 
     message = base64.b64encode(json.dumps(config))
     timestamp = str(int(math.floor(time.time())))
